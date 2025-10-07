00b01acd1380c0561505428b892181c8872ea607
naleesther <esthernalenyi@gmail.com>
2025-10-07 16:35:02 +0300

    changes on vite config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import vue from '@vitejs/plugin-vue'
 import tailwind from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-      vue(),
-    tailwind(),
+export default defineConfig( ({ command }) => ({
+  base: command === 'build'?'/Nalenyi-Portfolio/' : '/',
+  plugins:[vue(), tailwind()]
+}))
 
-  ],
-})
